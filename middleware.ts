84e9986be96ebe79dda6ebd8b8d4bdc178ede4ad
Bilal Muhammad <bilalmuhammad1240@gmail.com>
2026-09-01 23:16:45 +0200

    Adicionar assetlinks.json para verificação TWA

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|json)$).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|\\.well-known|.*\\.(?:svg|png|jpg|json)$).*)"],
 };
