3edd99e85451e539e0a7786c440aa281ba484386
Bilal Muhammad <bilalmuhammad1240@gmail.com>
2026-09-01 21:12:44 +0200

    Adicionar manifest PWA e ícones

---
+++ pwa-starterTilawah/android/app/src/main/java/com/bilal/tilawah/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bilal.tilawah;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ pwa-starterTilawah/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bilal.tilawah',
+  appName: 'pwa-starterTilawah',
+  webDir: 'dist'
+};
+
+export default config;
