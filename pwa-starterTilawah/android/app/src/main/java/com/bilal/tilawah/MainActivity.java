116c7382b190c158ba17a54706e62579cc29a81e
Bilal Muhammad <bilalmuhammad1240@gmail.com>
2026-09-01 22:35:23 +0200

    Remover definitivamente pastas de scaffold acidentais

--- pwa-starterTilawah/android/app/src/main/java/com/bilal/tilawah/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.bilal.tilawah;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- pwa-starterTilawah/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.bilal.tilawah',
-  appName: 'pwa-starterTilawah',
-  webDir: 'dist'
-};
-
-export default config;
